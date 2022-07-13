ed7d049ec76837ba9842abf4e9433c3f51baf976
bakemonostan <ehizomokhomion@gmail.com>
2022-07-13 05:52:16 -0700

    about section complted

--- components/About.jsx
+++ components/About.jsx
@@ -3,8 +3,11 @@ const About = () => {
     <div className='w-full p-2 py-16 md:h-screen flex items-center'>
       <div className='max-w-[1240px] m-auto md:grid grid-col-3 gap-8'>
         <div className='col-span-2'>
-          <h2 className='text-2xl'>About</h2>
-          <p>
+          <h2 className='text-2xl uppercase tracking-widest text-[#5651e5]'>
+            About
+          </h2>
+          <p className='py-4 font-bold text-4xl'>Who I Am</p>
+          <p className='py-2 text-gray-600'>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda
             animi laborum eum cupiditate sint quidem! Accusantium aliquid magni
             voluptas omnis ipsa vero doloribus consequatur qui rerum
@@ -12,7 +15,7 @@ const About = () => {
             officia blanditiis, nihil eos beatae. Asperiores tenetur distinctio
             magni accusantium ullam nesciunt, neque ex accusamus ad!
           </p>
-          <p>
+          <p className='py-2 text-gray-600'>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
             nesciunt, autem pariatur assumenda dolorem molestiae unde deleniti.
             Sunt, rem libero ullam non, adipisci animi officia odio cumque
@@ -20,7 +23,9 @@ const About = () => {
             ducimus, aspernatur dicta, possimus tenetur dolore sed facere
             excepturi sit? Quae voluptatum sequi ab iste itaque?
           </p>
-          <p>Check out some of my latest projects.</p>
+          <p className='py-2 text-gray-600 underline cursor-pointer'>
+            Check out some of my latest projects.
+          </p>
         </div>
       </div>
     </div>
