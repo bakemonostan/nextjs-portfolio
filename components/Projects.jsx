efcce6e1de11ffe300382e80c764475b6888b5c2
bakemonostan <ehizomokhomion@gmail.com>
2022-07-13 08:03:02 -0700

    project and projectItem components done

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,4 +1,21 @@
+import ProjectItem from './ProjectItem';
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className='w-full'>
+      <div className=' max-w-[1240px] mx-auto px-2 py-16'>
+        <p className='text-xl uppercase tracking-widest text-[#5651e5]'>
+          Projects
+        </p>
+        <h2 className='py-4'>What I have Built</h2>
+        <div className='grid md:grid-cols-2 gap-8'>
+          <ProjectItem />
+          <ProjectItem />
+          <ProjectItem />
+          <ProjectItem />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Projects;
