2d3b559bb7f00d1a3fac3403940acf2c0debc9a3
bakemonostan <ehizomokhomion@gmail.com>
2022-07-13 01:48:54 -0700

    added fonts to _document file and started main component

---
+++ components/Main.jsx
@@ -0,0 +1,24 @@
+const Main = () => {
+  return (
+    <div className='w-full h-screen text-center'>
+      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+        <div>
+          <p className='uppercase text-sm tracking-widest text-gray-600'>
+            Let&apos;s build something together
+          </p>
+          <h1 className='py-4 text-gray-700'>
+            Hi I&apos;m <span className='text-[#5651e5]'>Ehiz</span>
+          </h1>
+          <h2 className='py-2 text-gray-700'>A Front-End Web Developer</h2>
+          <p className='py-4 text-gray-600 m-auto max-w-[70%]'>
+            I am a front-end web developer specializing in building exceptional
+            and scalable digital experience. Currently, I am focused on building
+            responsive front-end web applications while learning back-end
+            technologies.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Main;
